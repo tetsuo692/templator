c24afa62337392205ef73b0215efbf20415719ca
Guillaume BELLEGUIC <guillaume.belleguic@cixi.eu>
2009-04-27 15:32:23 +0200

    create lib and templates

---
+++ templates/git_template.rb
@@ -0,0 +1,14 @@
+GIT_IGNORE_DIR = %w{tmp log vendor}
+GIT_INGORE_FILES = %w{log/*.log tmp/**/* config/database.yml db/*.sqlite3 .DS_STORE doc/api doc/app}
+
+
+git :init
+
+GIT_IGNORE_DIR.each { |directory| file "#{directory}/.gitignore"  } 
+file ".gitignore", GIT_INGORE_FILES.join("/\n")
+
+git :add => '.'
+
+git :commit => "-a -m 'Initial commit'"
+
+
--- templator.rb
+++ templator.rb
@@ -1,3 +1,7 @@
-inside('vendor') do
-  git :clone => "git://github.com/tetsuo692/templator.git" 
-end
+TEMPLATOR_ROOT="#{root}/vendor/templator"
+
+git :clone => "git://github.com/tetsuo692/templator.git #{TEMPLATE_ROOT}"
+
+require "#{TEMPLATOR_ROOT}/lib/helper/common_helper"
+
+yes?("Should I use git as scm ?") ? load_template("#{TEMPLATE_ROOT}/templates/git_template.rb") : load_template("#{TEMPLATE_ROOT}/base_template.rb")
