dfdacb46a99c32654785ff6803f85a996995b175
Guillaume BELLEGUIC <guillaume.belleguic@cixi.eu>
2009-04-28 11:48:03 +0200

    set rspec as default test framework

--- lib/templates/test_template.rb
+++ lib/templates/test_template.rb
@@ -1,4 +1,4 @@
 ask_list("Which test framework should I use ?",
           {:unit => Proc.new{""},
            :rspec => Proc.new{load_template("#{TEMPLATOR_TEMPLATES}/test/rspec_template.rb")},
-           :shoulda => Proc.new{load_template("#{TEMPLATOR_TEMPLATES}/test/shoulda_template.rb")}}, :default => :unit)
+           :shoulda => Proc.new{load_template("#{TEMPLATOR_TEMPLATES}/test/shoulda_template.rb")}}, :default => :rspec)
--- templator.rb
+++ templator.rb
@@ -9,11 +9,11 @@ git :clone => "git://github.com/tetsuo692/templator.git #{TEMPLATOR_ROOT}"
 
 require "#{TEMPLATOR_LIB}/helper/common_helper"
 
-# yes?("Should I use git as scm ?") ? load_template("#{TEMPLATOR_ROOT}/templates/git_template.rb") : load_template("#{TEMPLATOR_ROOT}/base_template.rb")
 
 ask_list("Which scm should I use ?",
           {:git => Proc.new{load_template("#{TEMPLATOR_TEMPLATES}/git_template.rb")},
            :none => Proc.new{load_template("#{TEMPLATOR_TEMPLATES}/base_template.rb")}}, :default => :git)
            
 log 'clean up'
+
 FileUtils.rm_r(TEMPLATOR_ROOT)
