d2e82af256c6518154abb3072c7a8a03c3427708
Guillaume BELLEGUIC <guillaume.belleguic@cixi.eu>
2009-04-28 10:10:03 +0200

    syntax bug

--- templates/test_template.rb
+++ templates/test_template.rb
@@ -1,4 +1,4 @@
 ask_list("Which test framework should I use ?",
           {:unit => Proc.new{""},
            :rspec => Proc.new{load_template("#{TEMPLATOR_ROOT}/templates/test_template/rspec_template.rb")},
-           :shoulda => Proc.new{load_template("#{TEMPLATOR_ROOT}/templates/test_template/shoulda_template.rb")}, :default => :unit)
+           :shoulda => Proc.new{load_template("#{TEMPLATOR_ROOT}/templates/test_template/shoulda_template.rb")}}, :default => :unit)
