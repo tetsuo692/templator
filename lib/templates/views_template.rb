fe49af7fc2152ab52173cbfc556d38a1b22c11c1
Guillaume BELLEGUIC <guillaume.belleguic@cixi.eu>
2009-04-28 11:24:26 +0200

    synthax errors

--- lib/templates/views_template.rb
+++ lib/templates/views_template.rb
@@ -1,3 +1,3 @@
 ask_list("Which views framework should I use ?",
           {:standard => Proc.new{""},
-           :haml => Proc.new{load_template("#{TEMPLATOR_TEMPLATES}/views/haml_template.rb")}, :default => :standard)
+           :haml => Proc.new{load_template("#{TEMPLATOR_TEMPLATES}/views/haml_template.rb")}}, :default => :standard)
